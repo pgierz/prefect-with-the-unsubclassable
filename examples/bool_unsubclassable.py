9b713c08f1dc0201a91a6e2cbf0f596e4f82fd08
Paul Gierz <pgierz@awi.de>
2025-02-06 12:51:33 +0100

    wip 2

--- examples/bool_unsubclassable.py
+++ examples/bool_unsubclassable.py
@@ -1,26 +1,33 @@
+from typing import Any
+
 from prefect import flow, task
 
 
-class BooleanWithProvenance:
-    def __init__(self, value, provenance):
+class BoolWithProvenance(int):
+    # NOTE(PG): Subclass from int to avoid TypeError when subclassing from bool. Bools are just
+    #           a special case of integers, so this should be fine.
+    def __new__(cls, value, provenance):
+        return super().__new__(cls, value)
+
+    def __init__(self, value: Any, provenance: list):
         self.value = value
         self._provenance = provenance
 
     @property
-    def provenance(self):
+    def provenance(self) -> list:
         return self._provenance
 
     @provenance.setter
-    def provenance(self, value):
+    def provenance(self, value: list):
         self._provenance = value
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return bool(self.value)
 
-    def __repr__(self):
-        return f"BooleanWithProvenance({self.value}, {self.provenance})"
+    def __repr__(self) -> str:
+        return f"BoolWithProvenance({self.value}, {self.provenance})"
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if isinstance(other, self.__class__):
             return self.value == other.value
 
@@ -40,11 +47,18 @@ def bool_task(x: bool) -> bool:
     return x
 
 
+@task
+def check_if_bool(x: Any) -> bool:
+    return isinstance(x, bool)
+
+
 @flow
 def main():
-    argument = BooleanWithProvenance(True, [])
+    argument = BoolWithProvenance(True, [])
     result = bool_task(argument)
     print(f"Result: {result}")
+    is_bool = check_if_bool(result)
+    print(f"Result is boolean-ish? {is_bool}")
 
 
 if __name__ == "__main__":
